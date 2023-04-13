f45432d3b532a5ea995b577f1d9d83ecbbddd72a
hieunoobpro <hieudsgh@gmail.com>
2023-04-13 12:26:10 +0700

    sua

--- src/handle/ClassHandle.java
+++ src/handle/ClassHandle.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 public class ClassHandle {
     public Classes classInput(Scanner scanner, int i){
         System.out.println("Nhap ten hoc sinh thu "+ (i+1) +" :" );
-        String name = scanner.nextLine();
+        String nameOfStudent = scanner.nextLine();
         System.out.println("Nhap tuoi: ");
-        int age = Integer.parseInt(scanner.nextLine());
+        int ageOfstudent = Integer.parseInt(scanner.nextLine());
         System.out.println("Nhap hoc luc: ");
-        String rank = scanner.nextLine();
+        String rankOfStudent = scanner.nextLine();
         System.out.println("Nhap mon hoc: ");
-        String subject = scanner.nextLine();
-        return new Classes(name, age, rank, subject, i);
+        String subjectOfStudent = scanner.nextLine();
+        return new Classes(nameOfStudent, ageOfstudent, rankOfStudent, subjectOfStudent, i);
     }
 }
